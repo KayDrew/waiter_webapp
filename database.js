6eacf7402169acac2592a4d5e4232d4b2a72ad5d
KayDrew <kdtshehla@gmail.com>
2023-09-07 01:54:21 -0700

    added routes

--- database.js
+++ database.js
@@ -1,6 +1,24 @@
 export default function queries(db){
 
+async function create(){
+
+  await  db.none(`CREATE TABLE IF NOT EXISTS days(monday VARCHAR(255),tuesday VARCHAR(255),wednsday VARCHAR(255),
+  thursday VARCHAR(255),friday VARCHAR(255),saturday VARCHAR(255),sunday VARCHAR(255));
+ `)
+
+ await  db.none(`CREATE TABLE IF NOT EXISTS waiters(name VARCHAR(255);`);
+
+}
 
 
+async function reset(){
 
+}
+
+
+{
+
+    create,
+    reset
+}
 }
