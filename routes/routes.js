755511265a30bd902ab6cd9b0484485299a71652
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-09-12 14:12:07 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,10 @@ export default function routes(queries){
 
 
     async function home(req,res,next){
+    
 
-        res.render("index",{
+    
+       res.render("index",{
 
         });
 
@@ -11,7 +13,7 @@ export default function routes(queries){
 
     
     async function admin(req,res,next){
-
+  await queries.getWaiters();
         res.render("admin",{
 
         });
@@ -20,17 +22,32 @@ export default function routes(queries){
 
     
     async function waiters(req,res,next){
+    	
+let username= req.params.username;
 
+ await queries.recordWaiters(username);
+ 
         res.render("waiters",{
 
         });
 
     }
 
+async  function clearSchedule(req,res,next){
+
+try{
+await queries.reset();
+res.redirect("/admin");
+}catch(err){
+
+console.log(err);
+}
+}
 
     return{
         home,
         admin,
-        waiters
+        waiters,
+        clearSchedule
     }
-}
+                           }
