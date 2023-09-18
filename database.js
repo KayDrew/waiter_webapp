d221466f5079676f89442bb8019cbcae0c0fa131
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-09-18 17:39:43 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,29 +1,13 @@
 export default function queries(db){
 
-async function create(){
 
-try{
-	
-	
-	//let result=await db.manyOrNone("\dt");
-	//db.none("DROP TABLE  admin");
-db.none("CREATE TABLE admin(dayID INT,name VARCHAR(255), FOREIGN KEY(dayID) REFERENCES days(dayID))");
-
-console.log("created");
-
-}catch(err){
-
-console.log(err);
-}
-
-}
 async function recordDays(id,day){
 
 try{
 	
 	
 	await db.none(`INSERT INTO days(dayID,day) VALUES ($1,$2)`,[id,day]);
-console.log("successfully inserted "+day);
+
 
 
 	
@@ -42,20 +26,17 @@ try{
 	
 	if(waiter){
 	
-	await db.none(`INSERT INTO waiters(waiterID,name) VALUES (DEFAULT,$1)`,waiter);
-console.log("successfully inserted "+waiter);
-}
+	 await db.none(`INSERT INTO waiters(waiterID,name) VALUES (DEFAULT,$1)`,waiter);
 
-else{
-console.log("user undefined");
+       }
 
-}
+   
 	
-}catch(err){
+   }catch(err){
 	
-console.log(err);
+         console.log(err);
 
-}
+  }
 
 }
 
@@ -66,10 +47,10 @@ async function getAdmin(){
 try{
 	
 	let result=await db.manyOrNone(`SELECT admin.name, days.day FROM admin JOIN days ON admin.dayID=days.dayID`);
-	console.log(result);
+	
 	
 	return result;
-}catch(err){
+  }catch(err){
 
 console.log(err);
 }
@@ -80,9 +61,9 @@ console.log(err);
 async function reset(){
 
 try{
-await db.none(`DELETE FROM waiters`);
-await db.none(`DELETE FROM admin`);
-console.log("successfully  deleted!");
+     await db.none(`DELETE FROM waiters`);
+     await db.none(`DELETE FROM admin`);
+
 }catch(err){
 
 console.log(err);
@@ -93,40 +74,38 @@ async function setAdmin(dayID,waiter){
 
 try{
 
-db.none("INSERT INTO admin(dayID,name) VALUES ($1,$2)",[dayID,waiter]);
-
-console.log("waiterID:" + waiter+" and day: "+dayID+" inserted");
-
+     db.none("INSERT INTO admin(dayID,name) VALUES ($1,$2)",[dayID,waiter]);
 
 } catch(err){
 console.log(err);
 
+ }
 }
-}
+	
 
 async function update(waiter){
 
 if(waiter){
 	
-try{
+ try{
 	
 	await db.none("DELETE  FROM waiters WHERE name=$1",waiter);
 	await db.none("DELETE FROM admin WHERE name=$1",waiter);
 	console.log("updated");
 	
-}catch(err){
+  }catch(err){
 
-console.log(err);
-}
+     console.log(err);
+  }
 
-}
+ }
 
 }
 
 
 
 return{
-create,
+
     reset,
     recordDays,
     recordWaiters,
@@ -134,5 +113,5 @@ create,
     update,
     setAdmin,
     
+    }
 }
-	}
