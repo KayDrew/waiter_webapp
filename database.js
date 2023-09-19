0f3d072cef5683b7e911364c14c478d51f107c30
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-09-19 18:09:02 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,12 @@
 export default function queries(db){
 
-
 async function recordDays(id,day){
 
 try{
 	
-	
 	await db.none(`INSERT INTO days(dayID,day) VALUES ($1,$2)`,[id,day]);
 
-
-
-	
-}catch(err){
+ }catch(err){
 	
 console.log(err);
 
@@ -21,39 +16,32 @@ console.log(err);
 
 
 async function recordWaiters(waiter){
-
-try{
 	
-	if(waiter){
+  if(waiter){
 	
-	 await db.none(`INSERT INTO waiters(waiterID,name) VALUES (DEFAULT,$1)`,waiter);
-
-       }
-
-   
+   try{
 	
-   }catch(err){
+	  await db.none(`INSERT INTO waiters(waiterID,name) VALUES (DEFAULT,$1)`,waiter);
+     }catch(err){
 	
-         console.log(err);
-
+     console.log(err);
+      }
   }
-
 }
 
 
 async function getAdmin(){
 
 
-try{
+  try{
 	
-	let result=await db.manyOrNone(`SELECT admin.name, days.day FROM admin JOIN days ON admin.dayID=days.dayID`);
-	
-	
-	return result;
+	  let result=await db.manyOrNone(`SELECT admin.name, days.day FROM admin JOIN days ON admin.dayID=days.dayID`);
+
+	  return result;
   }catch(err){
 
-console.log(err);
-}
+   console.log(err);
+ }
 
 }
 
@@ -61,8 +49,8 @@ console.log(err);
 async function reset(){
 
 try{
-     await db.none(`DELETE FROM waiters`);
-     await db.none(`DELETE FROM admin`);
+await db.none(`DELETE FROM waiters`);
+await db.none(`DELETE FROM admin`);
 
 }catch(err){
 
@@ -74,44 +62,59 @@ async function setAdmin(dayID,waiter){
 
 try{
 
-     db.none("INSERT INTO admin(dayID,name) VALUES ($1,$2)",[dayID,waiter]);
+db.none("INSERT INTO admin(dayID,name) VALUES ($1,$2)",[dayID,waiter]);
 
 } catch(err){
+	
 console.log(err);
 
- }
 }
-	
+}
 
 async function update(waiter){
 
 if(waiter){
 	
- try{
+try{
 	
 	await db.none("DELETE  FROM waiters WHERE name=$1",waiter);
 	await db.none("DELETE FROM admin WHERE name=$1",waiter);
-	console.log("updated");
 	
-  }catch(err){
+	
+}catch(err){
 
-     console.log(err);
-  }
+console.log(err);
+}
 
- }
+}
 
 }
 
 
+async  function  getWaiter(){
+
+		
+try{
+
+let result=db.manyOrNone("SELECT name FROM waiters");
+return result;
+
+}catch(err){
+	
+console.log(err);
+
+}
+
+}
+
 
 return{
-
     reset,
     recordDays,
     recordWaiters,
     getAdmin,
     update,
     setAdmin,
-    
+    getWaiter    
     }
 }
